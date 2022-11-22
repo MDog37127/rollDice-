2745933f7bbbf88f5ca5c258221ec652051e4bdd
MDog37127 <bentleymdog@gmail.com>
2022-11-21 18:28:35 -0600

    Initial Commit

---
+++ rollDice()/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rollDice()
+//
+//  Created by McLennan Jones on 11/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
